f8c255db1e2dfca874baca13d4b1c17d462152fe
Manish Pingale <manishpingale2005@gmail.com>
2024-12-04 14:49:46 +0530

    Day 4: 04/12/2024 Quick Sort

---
+++ Algorithms/Quiclk_sort.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+
+    while (i<j)
+    {
+        while (i < high && arr[i] <= pivot)    //Finding 1st bigger element than pivot
+            i++;
+        
+        while (j > low && arr[j] > pivot)      //Finding 1st smaller element than pivot
+            j--;
+
+        if(i<j)        
+            swap(arr[i],arr[j]);
+    }
+
+    swap(arr[low],arr[j]);
+    return j;
+}
+
+void quickSorty(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int partitionInd = partition(arr, low, high);
+        quickSorty(arr, low, partitionInd - 1);
+        quickSorty(arr, partitionInd + 1, high);
+    }
+}
+
+int main()
+{
+    int arr[] = {13, -41,-5, 0, 4, 5};
+    cout<<"Before:\n"; 
+    for(auto i:arr)
+        cout<<i<<endl;
+
+    quickSorty(arr, 0, 5);
+    
+    cout<<"After:\n"; 
+    for(auto i:arr)
+        cout<<i<<endl;
+    return 0;
+}
